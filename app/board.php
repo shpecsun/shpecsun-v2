5a2f05315b824dd7e76742e70dfae60489c55e21
Alejandro Martinez^2 <alejandro.martinez@metalab.csun.edu>
2016-09-13 23:43:53 -0700

    board

--- app/board.php
+++ app/board.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class board extends Model
+class Board extends Model
 {
     protected $table = 'board_members';
     protected $primaryKey = 'id';
